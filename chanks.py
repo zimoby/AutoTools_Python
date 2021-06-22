e791521a0f7bda2162917b85083e8fc52b7788d5
ZMac <zimoby@gmail.com>
2021-06-22 20:34:18 +0300

    chanks and projOrg update

--- chanks.py
+++ chanks.py
@@ -9,9 +9,9 @@ def dopChankFunc(inputMin):
         dopChank = 0
     return dopChank
 
-def hourMinCount(hours, koef, chank):
+def hourMinCount(hours, factor, chank):
     h = hours
-    k = koef
+    k = factor
     ch = chank
 
     t = h * k
@@ -23,30 +23,28 @@ def hourMinCount(hours, koef, chank):
     return export
     
 
-def chankFunction(timeIn, a):
+def chankFunction(timeIn, a, factorArr, chank):
     stand = timeIn
-    chank = 25
-    a = a
 
     hours = stand - a
 
     print("Ease mod")
-    ex = hourMinCount(hours, 0.25, chank)
-    print("hours:", ex[0], " min:", ex[1])
+    ex = hourMinCount(hours, factorArr[0], chank)
+    print("hours:", str(ex[0]) + ":" + str(ex[1]))
     print("Chanks:", ex[2])
     ch1 = ex[2]
     print()
 
     print("Standart mod")
-    ex = hourMinCount(hours, 0.5, chank)
-    print("hours:", ex[0], " min:", ex[1])
+    ex = hourMinCount(hours, factorArr[1], chank)
+    print("hours:", str(ex[0]) + ":" + str(ex[1]))
     print("Chanks:", ex[2])
     ch2 = ex[2]
     print()
 
     print("Hard mod")
-    ex = hourMinCount(hours, 0.75, chank)  
-    print("hours:", ex[0], " min:", ex[1])
+    ex = hourMinCount(hours, factorArr[2], chank)  
+    print("hours:", str(ex[0]) + ":" + str(ex[1]))
     print("Chanks:", ex[2])
     ch3 = ex[2]
     print()
@@ -64,27 +62,24 @@ def inputTime(textTime):
 mode = input("d - day, h - hours of work, l - limit\nMode: ")
 
 startHour = 14
-endHour = 24 + 3
+endHour = 24 + 4
+factorArr = [0.25, 0.5, 0.75]
+chank = 25
 
 if mode == "d":
-    print("до", endHour % 24, "часов ночи:", "\n")
+    print("до", endHour % 24, "часов ночи:")
     startHour = inputTime("current time")
-    x = chankFunction(endHour, startHour)
+    print("осталось", abs(endHour % 24 - startHour), "часов", "\n")
+    x = chankFunction(endHour, startHour, factorArr, chank)
 elif mode == "h":
-    print("до 4 часов ночи:", "\n")
-    x = chankFunction(endHour, startHour)
+    print("до", endHour % 24, "часов ночи:", "\n")
     b = int(input("hours: "))
     for i in x:
         xx = ceil(b/ (i/2))
         print("days of work:", xx, " chanks for day:", i)
 elif mode == "l":
+    print("до", endHour % 24, "часов ночи:")
     startHour = inputTime("current time")
     endHour = inputTime("end time")
-    x = chankFunction(endHour, startHour)
-
-
-# print("до 2 часов ночи:", "\n")
-# chankFunction(26, a)
-
-# print("до 6 часов ночи:", "\n")
-# chankFunction(30, a)
+    print("осталось", abs(endHour % 24 - startHour), "часов", "\n")
+    x = chankFunction(endHour, startHour, factorArr, chank)
--- projOrg.py
+++ projOrg.py
@@ -1,10 +1,17 @@
 import sys
 import os
-# from pathlib import Path
+
+foldersDef = [
+    ["Assets",
+        "Frames", "Fonts"],
+    "Export",
+    "Projects",
+    "Proxy",
+    "Share"
+]
 
 def createFolder(newPath):
     if not os.path.exists(newPath):
-        # print(newPath)
         os.mkdir(newPath)
 
 def parseFolders(arr, path, iter):
@@ -12,92 +19,32 @@ def parseFolders(arr, path, iter):
         if isinstance(arr[y], list):
             parentFolder = os.path.join(path, arr[y][0])
             print("New folder: " + parentFolder)
-            # print(parentFolder)
             createFolder(parentFolder)
             parseFolders(arr[y], parentFolder, 1)
         else:
             subFolder = os.path.join(path, arr[y])
             print("New folder: " + subFolder)
-            # print(parentFolder)
             createFolder(subFolder)
 
 argumentsNum = len(sys.argv)
-path = ""
-type = ""
+foldersType = ""
 
 if argumentsNum == 3:
     if sys.argv[1]:
-        type = sys.argv[1]
+        foldersType = sys.argv[1]
     if sys.argv[2]:
-        if os.path.normpath(sys.argv[2][-1]) == "/" or os.path.normpath(sys.argv[2][-1]) == "\\":
+        lastPathChar = os.path.normpath(sys.argv[2][-1])
+        if lastPathChar == "/" or lastPathChar == "\\":
             path = os.path.normpath(sys.argv[2][0:-1])
         else:
             path = os.path.normpath(sys.argv[2])
     else:
         path = ""
 
-if type == "def":
-    folders = [
-        ["Assets",
-            "Frames", "Fonts"],
-        "Export",
-        "Projects",
-        "Proxy",
-        "Share"
-    ]
+if foldersType == "def":
+    foldersStructure = foldersDef
 
 if path != "":
-    parseFolders(folders, path, 0)
+    parseFolders(foldersStructure, path, 0)
 else:
     print("Path not found")
-
-
-
-# for y in folder:
-#     # newPath = path + "\\" + y[0]
-#     if len(y) > 0:
-#         parseFolders(y, path)
-
-#     newPath = path + "\\" + y[0]
-#     # createFolder(newPath)
-#     # if len(y) == 1:
-#         # if not os.path.exists(newPath):
-#         #     os.mkdir(newPath)
-#         # print(newPath)
-#     # else:
-#     for x in range(1, len(y)):
-#         newPath = path + "\\" + y[0] + "\\" + y[x]
-#         # print(newPath)
-#         createFolder(newPath)
-
-
-    # for x in range(argumentsNum):
-        # print(x, sys.argv[x])
-        # if 
-        # if sys.argv[1]:
-            # arg = 
-            # alert(path.split(" "))
-        #     path = sys.argv[1]
-        # else:
-        #     path = "Path not found"
-
-        # if sys.argv[2]:
-        #     type = sys.argv[2]
-        # else:
-        #     type = "default"
-
-# print(path)
-
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                     help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                     const=sum, default=max,
-#                     help='sum the integers (default: find the max)')
-
-# args = parser.parse_args()
-
-
-# parser.add_argument("file_path", type=Path)
-
-# p = parser.parse_args()
